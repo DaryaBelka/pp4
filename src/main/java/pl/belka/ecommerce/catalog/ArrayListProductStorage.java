25f7dddb93503afe4bcd31ea65327a51b6475192
s-301-15 <s-301-15@gumiforest.uek.krakow.pl>
2024-04-08 16:32:15 +0200

    zad

---
+++ src/main/java/pl/belka/ecommerce/catalog/ArrayListProductStorage.java
@@ -0,0 +1,31 @@
+package pl.belka.ecommerce.catalog;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListProductStorage implements ProductStorage {
+
+    private ArrayList<Product> products;
+
+    public ArrayListProductStorage(){
+        products = new ArrayList<>();
+    }
+
+    @Override
+    public List<Product> allProducts() {
+        return products;
+    }
+
+    @Override
+    public void add(Product newProduct){
+        products.add(newProduct);
+    }
+
+    @Override
+    public Product getProductBy(String id) {
+        return products.stream()
+                .filter(product -> product.getId().equals(id))
+                .findFirst()
+                .get();
+    }
+}
